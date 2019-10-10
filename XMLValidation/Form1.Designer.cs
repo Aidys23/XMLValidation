d0eee643805220c907aba38086b757cbfbf95876
Aidys23 <aidyssalchak@gmail.com>
2019-10-10 20:14:19 +0700

    Добавьте файлы проекта.

---
+++ XMLValidation/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace XMLValidation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.open = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.SaveTo = new System.Windows.Forms.Button();
+            this.deleteLog = new System.Windows.Forms.Button();
+            this.OpenLog = new System.Windows.Forms.Button();
+            this.WorkDirectory = new System.Windows.Forms.Label();
+            this.FilePath = new System.Windows.Forms.Label();
+            this.XMLViewer = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(13, 13);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(75, 23);
+            this.open.TabIndex = 0;
+            this.open.Text = "open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // save
+            // 
+            this.save.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.save.Location = new System.Drawing.Point(94, 12);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 1;
+            this.save.Text = "save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // SaveTo
+            // 
+            this.SaveTo.Location = new System.Drawing.Point(175, 13);
+            this.SaveTo.Name = "SaveTo";
+            this.SaveTo.Size = new System.Drawing.Size(75, 23);
+            this.SaveTo.TabIndex = 2;
+            this.SaveTo.Text = "SaveTo";
+            this.SaveTo.UseVisualStyleBackColor = true;
+            this.SaveTo.Click += new System.EventHandler(this.SaveTo_Click);
+            // 
+            // deleteLog
+            // 
+            this.deleteLog.Location = new System.Drawing.Point(772, 13);
+            this.deleteLog.Name = "deleteLog";
+            this.deleteLog.Size = new System.Drawing.Size(75, 23);
+            this.deleteLog.TabIndex = 3;
+            this.deleteLog.Text = "Delete Log";
+            this.deleteLog.UseVisualStyleBackColor = true;
+            this.deleteLog.Click += new System.EventHandler(this.deleteLog_Click);
+            // 
+            // OpenLog
+            // 
+            this.OpenLog.Location = new System.Drawing.Point(853, 13);
+            this.OpenLog.Name = "OpenLog";
+            this.OpenLog.Size = new System.Drawing.Size(75, 23);
+            this.OpenLog.TabIndex = 4;
+            this.OpenLog.Text = "Open Log";
+            this.OpenLog.UseVisualStyleBackColor = true;
+            this.OpenLog.Click += new System.EventHandler(this.OpenLog_Click);
+            // 
+            // WorkDirectory
+            // 
+            this.WorkDirectory.AutoSize = true;
+            this.WorkDirectory.Location = new System.Drawing.Point(256, 9);
+            this.WorkDirectory.Name = "WorkDirectory";
+            this.WorkDirectory.Size = new System.Drawing.Size(0, 13);
+            this.WorkDirectory.TabIndex = 5;
+            // 
+            // FilePath
+            // 
+            this.FilePath.AutoSize = true;
+            this.FilePath.Location = new System.Drawing.Point(256, 26);
+            this.FilePath.Name = "FilePath";
+            this.FilePath.Size = new System.Drawing.Size(0, 13);
+            this.FilePath.TabIndex = 6;
+            // 
+            // XMLViewer
+            // 
+            this.XMLViewer.Location = new System.Drawing.Point(12, 42);
+            this.XMLViewer.Name = "XMLViewer";
+            this.XMLViewer.Size = new System.Drawing.Size(916, 668);
+            this.XMLViewer.TabIndex = 7;
+            this.XMLViewer.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 722);
+            this.Controls.Add(this.XMLViewer);
+            this.Controls.Add(this.FilePath);
+            this.Controls.Add(this.WorkDirectory);
+            this.Controls.Add(this.OpenLog);
+            this.Controls.Add(this.deleteLog);
+            this.Controls.Add(this.SaveTo);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Closing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button SaveTo;
+        private System.Windows.Forms.Button deleteLog;
+        private System.Windows.Forms.Button OpenLog;
+        private System.Windows.Forms.Label WorkDirectory;
+        private System.Windows.Forms.Label FilePath;
+        private System.Windows.Forms.RichTextBox XMLViewer;
+    }
+}
+
---
+++ XMLValidation/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Xml.Schema;
+using System.Diagnostics;
+
+namespace XMLValidation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeComponent();
+            string current = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+            WorkDirectory.Text = current;
+            if (!File.Exists(current + "/Log.txt"))
+            {
+                var myFile = File.Create(current + "/Log.txt");
+                myFile.Close();
+                XMLViewer.AppendText("Aloha");
+            }
+
+            Log("Application launched!");
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "XML Files (*.xml)|*.xml";
+            ofd.FilterIndex = 0;
+            ofd.DefaultExt = "xml";
+            ofd.Multiselect = false;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    XmlReaderSettings bookSet = new XmlReaderSettings();
+                    bookSet.Schemas.Add("urn:cbr-ru:ed:v2.0", WorkDirectory.Text + "/XSDFiles/cbr_ed542_v2019.2.0.xsd");
+                    bookSet.ValidationType = ValidationType.Schema;
+                    bookSet.ValidationEventHandler += new ValidationEventHandler(bookSetValidateEventHandler);
+
+                    bool errors = false;
+                    XmlReader book = XmlReader.Create(ofd.FileName, bookSet);
+                    XmlDocument document = new XmlDocument();
+                    Log("Trying to validate " + ofd.FileName);
+                    document.Load(book);
+                    document.Validate((o, er) => { if (er.Severity == XmlSeverityType.Error) errors = true; });
+
+                    MessageBox.Show(String.Format(Path.GetFileName(ofd.FileName) + ": {0} ", errors ? "Did not validate. You can't work with that file" : "Successfully validated"), "XML Document Validation", MessageBoxButtons.OK, errors ? MessageBoxIcon.Error : MessageBoxIcon.Information);
+                    if (!errors)
+                    {
+
+                        XMLViewer.Text = document.InnerXml;
+                        FilePath.Text = ofd.FileName;
+                    }
+                    Log(String.Format(Path.GetFileName(ofd.FileName) + ": {0} ", errors ? "Did not validate. You can't work with that file" : "Successfully validated"));
+                    book.Close();
+                    ofd.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        static void bookSetValidateEventHandler(object sender, ValidationEventArgs e)
+        {
+            if (e.Severity == XmlSeverityType.Warning)
+            {
+                Log("WARNING: " + e.Message);
+            }
+            else if (e.Severity == XmlSeverityType.Error)
+            {
+                Log("Error: " + e.Message);
+            }
+        }
+
+        static void Log(String message)
+        {
+            try
+            {
+                string current = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+                File.AppendAllText(current + "/Log.txt", DateTime.Now + " : " + message + "\n", System.Text.Encoding.UTF8);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SaveTo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "XML Files (*.xml)|*.xml";
+                sfd.FilterIndex = 0;
+                sfd.DefaultExt = "xml";
+                sfd.RestoreDirectory = true;
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(sfd.FileName, XMLViewer.Text);
+                    MessageBox.Show("File saved");
+                }
+                Log("New file " + sfd.FileName + " saved!");
+                sfd.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.WriteAllText(FilePath.Text, XMLViewer.Text);
+                MessageBox.Show("File saved");
+                Log("File " + FilePath.Text + " has been changed!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void deleteLog_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.Delete(WorkDirectory.Text + "/Log.txt");
+                MessageBox.Show("File deleted");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void OpenLog_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("notepad.exe", WorkDirectory.Text + "/Log.txt");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void Closing(object sender, FormClosingEventArgs e)
+        {
+            Log("Application closed!\n");
+        }
+    }
+}
